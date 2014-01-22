0410851d140af8834c80647f75b1bac5d8ddea2c
Agolveo <agolveo@gmail.com>
2014-01-21 16:44:43 -0800

    user rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do 
+  	it 'is valid' do 
+  		expect(user).to be_valid
+  	end
+
+  	it 'requires an email' do
+  		user.email = nil
+  		expect(user).to be_invalid
+  	end
+
+  	it 'requires a username' do 
+  		user.username = nil
+  		expect(user).to be_invalid
+  	end
+
+  	it 'requires a uid' do 
+  		user.uid = nil
+  		expect(user).to be_invalid
+  	end
+
+  	it 'requires a provider' do 
+  		user.provider = nil
+  		expect(user).to be_invalid
+  	end
 end
