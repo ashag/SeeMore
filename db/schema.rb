6570a1d81649ad866b2df41c4c91f089807939dc
Agolveo <agolveo@gmail.com>
2014-01-27 12:40:26 -0800

    add db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 20140124203200) do
   end
 
   create_table "user_feeds", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "feed_id"
+    t.string   "user_id"
+    t.string   "feed_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
