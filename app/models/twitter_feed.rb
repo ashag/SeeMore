8ed1010d71732396507794fe6ed3a901bb09ea4f
shanghaiellen@gmail.com <shanghaiellen@gmail.com>
2014-01-28 10:07:30 -0800

    refactored content for twitter_feed into html"

--- app/models/twitter_feed.rb
+++ app/models/twitter_feed.rb
@@ -1,13 +1,12 @@
 class TwitterFeed < Feed
 
   def find_or_create_post(feed_uid, post)
-    tweet_link = get_user_link(feed_uid)
-    # Write as HTML
-    content = "<% image_tag(#{}Feed.get_pic(feed_uid) %>
-              <%= link_to #{tweet_link[0]}, #{tweet_link[1]} %>
-              <%= #{post.text}%>"
-    date = post.created_at
     @feed = Feed.find_by(uid: feed_uid)
+    tweet_link = get_user_link(feed_uid)
+    content = "< img src='#{@feed.get_pic(feed_uid)} alt='avatar for #{tweet_link[0]}' >
+              < a href='#{tweet_link[1]}' > #{tweet_link[0]} </a>
+              #{post.text}"
+    date = post.created_at
     Post.find_by(content: content) || Post.create(
       content: content,
       feed_id: @feed.id,
@@ -15,17 +14,22 @@ class TwitterFeed < Feed
       feed_uid: feed_uid)
   end
 
-  def get_user_link(id)
-    get_tweeter(id.to_i)
+  def get_user_link(uid)
+    get_tweeter(uid.to_i)
     [@tweeter.screen_name, "https://www.twitter.com/#{@tweeter.screen_name}"]
   end
 
-  def get_tweeter(id)
-    @tweeter = TwitterFeed.client.user(id.to_i)
+  def get_pic(uid)
+    get_tweeter(uid.to_i)
+    @tweeter.profile_image_url
   end
 
-  def get_posts(id)
-    get_tweeter(id.to_i)
+  def get_tweeter(uid)
+    @tweeter = TwitterFeed.client.user(uid.to_i)
+  end
+
+  def get_posts(uid)
+    get_tweeter(uid.to_i)
     posts = []
     TwitterFeed.client.user_timeline(@tweeter).each do |tweet|
       posts << tweet
