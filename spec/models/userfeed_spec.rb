1d12c13d51283e4b76bf168bc818c47caccd984d
Agolveo <agolveo@gmail.com>
2014-01-29 15:52:38 -0800

    userfeed spec

--- spec/models/userfeed_spec.rb
+++ spec/models/userfeed_spec.rb
@@ -1,13 +1,28 @@
 require 'spec_helper'
 
 describe UserFeed do
+  let!(:asha) { User.create(username: 'asha', email: 'blah@gmail.com') }
+  let!(:feed) { Feed.create(uid: 'blah', type: 'RSSFeed') }
 
-  describe 'validations' do 
+  describe 'create_relationship method' do  
     it 'will create relationship between user and feed' do 
-      asha = User.new(username: 'asha', email: 'blah@gmail.com')
-      feed = Feed.new(uid: 'blah', type: 'RSSFeed') 
-      ashafeed = UserFeed.create(user_id: asha.id, feed_id: feed.id) 
+      ashafeed = UserFeed.create_relationship(feed, asha) 
       expect(ashafeed).to be_valid
     end
+  end
+
+  describe 'following method' do
+    let!(:ashafeed) { UserFeed.create_relationship(feed, asha) }
+    let!(:new_user) { User.create(username: 'person', email: 'person@gmail.com') }
+
+    it 'will return true' do 
+      find = UserFeed.following?(asha.id, feed.uid)
+      expect(find).to be_true
+    end
+
+    # it 'will return false' do
+    #   find = UserFeed.following?(new_user.id, feed.uid)
+    #   expect(find).to be_false
+    # end
   end
 end
