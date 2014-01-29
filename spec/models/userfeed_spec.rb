f7021d84bccffac1b01bf4486c076d230a260035
Agolveo <agolveo@gmail.com>
2014-01-29 14:38:33 -0800

    userfeed specs

--- spec/models/userfeed_spec.rb
+++ spec/models/userfeed_spec.rb
@@ -3,11 +3,16 @@ require 'spec_helper'
 describe UserFeed do
 
   describe 'validations' do 
+
+  end
+
+  describe 'create relationship method' do 
     it 'will create relationship between user and feed' do 
       asha = User.new(username: 'asha', email: 'blah@gmail.com')
       feed = Feed.new(uid: 'blah', type: 'RSSFeed') 
-      ashafeed = UserFeed.create(user_id: asha.id, feed_id: feed.id) 
+      ashafeed = UserFeed.create_relationship(asha, feed) 
       expect(ashafeed).to be_valid
     end
   end
+
 end
