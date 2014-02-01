324d6b080f411d04fa456bd12fb636fb5be8cf35
Agolveo <agolveo@gmail.com>
2014-01-31 16:16:02 -0800

    fixed more merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,6 @@
 SeeMore::Application.routes.draw do
 
   root 'welcome#index'
-<<<<<<< HEAD
-  get '/user/:id'     => 'users#show', as: :show_user
-
-  post '/feeds'       => 'feeds#create',      as: "create_feed"
-
-  get 'search'        => 'feeds#search', :as => 'search'
-  post 'feed_search'   => 'feeds#rss_feed', as: 'create_rss'
-  # get 'search', to: 'welcome#index'
-  get 'results'       => 'welcome#results', as: 'results'
-  get 'insta_search' => 'feeds#insta_search', as: 'search_ig'
-
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-  # for potential future user profile deletion
-  # get 'goodbye' => 'users#goodbye', as: :goodbye
-  # delete '/user/:id' => 'users#destroy'
-
-  get '/auth/twitter', as: :sign_in
-  get '/auth/:provider/callback', to: 'sessions#create'
-  post '/auth/:provider/callback', :to => 'sessions#create'
-  get '/auth/failure', :to => 'sessions#failure'
-  get 'sign_out' => 'sessions#destroy', as: :sign_out
-
-  post 'tweet' => 'posts#tweet', as: 'tweet'
-  post 'favorite' => 'posts#favorite', as: 'favorite'
-
-=======
   get  '/user/:id'   => 'users#show',       as: :show_user
   post '/feeds'      => 'feeds#create',     as: 'create_feed'
   get  'search'      => 'feeds#search',     as: 'search'
@@ -45,5 +17,4 @@ SeeMore::Application.routes.draw do
 
   # This must be the last route listed, or every route afterwards will break
   get  '/:page',                            to: 'welcome#index'
->>>>>>> ffa9ee7e7c072e712e391e8aa60b4c8ff4b3e396
 end
